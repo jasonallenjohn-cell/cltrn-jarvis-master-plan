df43ae819a48140ab028f30298ea7a0d1d8b2024
jasonallenjohn-cell <jasonallenjohn@gmail.com>
2026-02-15 01:52:20 -0500

    fix: update .gitignore, remove committed build artifacts

--- apps/web/.next/server/server-reference-manifest.js
+++ apps/web/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"yuqP4Wg6LtDSDUnbdo9/16EfP88HWidhxrkKd5z9d3s=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"AF6HWOdKzyr4eA8T+yxskLCyElTZ5ZCtWzOCUyiAOMQ=\"}"
